9b4f7e7d22d2fb9a63c752460f690ed8ecfc7788
podrucki444 <118886547+OskarPodrucki@users.noreply.github.com>
2023-11-19 17:16:32 +0100

    Update uzytkownicy.sql

--- kursy.pasja.informatyki/kurs.php/php3/osadnicy/uzytkownicy.sql
+++ kursy.pasja.informatyki/kurs.php/php3/osadnicy/uzytkownicy.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.4.14
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Czas generowania: 28 Gru 2015, 18:21
--- Wersja serwera: 5.6.26
--- Wersja PHP: 5.6.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
