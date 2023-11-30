15b371ca0dc1e5663a5005f87c1a802cc1534f85
Oskar Podrucki <oskar.podrucki1@gmail.com>
2023-11-30 07:30:55 +0100

    ...

---
+++ kursy.net.ninja/kurs.vue.js/kurs.vue.5-modal.project/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  
+  <h1>{{ title }}</h1>
+
+  <Modal />
+  
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data() {
+    return {
+      title: 'My first Vue.js App :*'
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
