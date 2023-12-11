a46df8fee9650062762b9a9086970e764e085889
Oskar Podrucki <oskar.podrucki1@gmail.com>
2023-12-11 08:32:48 +0100

    ...

---
+++ 1.kurs.cpp/kurs.cpp.15/polska.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+string napis;
+
+main(){
+
+    // zmieni† typ kodowania na 852 ??
+
+    cin>>napis;
+    cout<<"Wpisany ci¥g znak¢w: "<<napis<<endl;
+    return 0;
+}
